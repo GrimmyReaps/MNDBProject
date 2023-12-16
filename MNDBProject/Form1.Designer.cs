00b4c221cfef30e97d5d87b584f1e3d25d52c2d1
GrimmyReaps <tomek1545@interia.pl>
2023-12-16 15:30:29 +0100

    Finished displaying of Clients and Movies.

--- MNDBProject/Form1.Designer.cs
+++ MNDBProject/Form1.Designer.cs
@@ -28,30 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.Close = new System.Windows.Forms.Button();
+            this.DebugLabel = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ShowClients = new System.Windows.Forms.Button();
+            this.ShowMovies = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // Close
             // 
-            this.button1.Location = new System.Drawing.Point(1105, 451);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(91, 34);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Zamknij";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Close.Location = new System.Drawing.Point(1105, 451);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(91, 34);
+            this.Close.TabIndex = 0;
+            this.Close.Text = "Zamknij";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
+            // DebugLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1079, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 16);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Empty Sadge T_T";
+            this.DebugLabel.AutoSize = true;
+            this.DebugLabel.Location = new System.Drawing.Point(1079, 9);
+            this.DebugLabel.Name = "DebugLabel";
+            this.DebugLabel.Size = new System.Drawing.Size(117, 16);
+            this.DebugLabel.TabIndex = 2;
+            this.DebugLabel.Text = "Empty Sadge T_T";
             // 
             // dataGridView1
             // 
@@ -64,14 +66,37 @@
             this.dataGridView1.TabIndex = 3;
             this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
             // 
+            // ShowClients
+            // 
+            this.ShowClients.Location = new System.Drawing.Point(1105, 399);
+            this.ShowClients.Name = "ShowClients";
+            this.ShowClients.Size = new System.Drawing.Size(91, 46);
+            this.ShowClients.TabIndex = 4;
+            this.ShowClients.Text = "Wyświetl Klientów";
+            this.ShowClients.UseVisualStyleBackColor = true;
+            this.ShowClients.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ShowMovies
+            // 
+            this.ShowMovies.Enabled = false;
+            this.ShowMovies.Location = new System.Drawing.Point(1105, 347);
+            this.ShowMovies.Name = "ShowMovies";
+            this.ShowMovies.Size = new System.Drawing.Size(91, 46);
+            this.ShowMovies.TabIndex = 5;
+            this.ShowMovies.Text = "Wyświetl Filmy";
+            this.ShowMovies.UseVisualStyleBackColor = true;
+            this.ShowMovies.Click += new System.EventHandler(this.ShowMovies_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1208, 497);
+            this.Controls.Add(this.ShowMovies);
+            this.Controls.Add(this.ShowClients);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DebugLabel);
+            this.Controls.Add(this.Close);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -82,9 +107,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Label DebugLabel;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button ShowClients;
+        private System.Windows.Forms.Button ShowMovies;
     }
 }
 
--- MNDBProject/Form1.cs
+++ MNDBProject/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,24 +16,31 @@ namespace MNDBProject
 {
     public partial class Form1 : Form
     {
-        MongoClient dbClient = null;
+        //Connect to Atlas
+        readonly MongoClient dbClient = new MongoClient("mongodb://localhost:27017");
+
         public Form1()
         {
-            dbClient = new MongoClient("mongodb://localhost:27017");
+            //Enter the Database 
             var mongoDatabase = dbClient.GetDatabase("Wypozyczalnia");
+            //Choose the Collection
             var mongoCollection = mongoDatabase.GetCollection<Movies>(Movies.MoviesDBName);
             //var document = mongoCollection.Find(new BsonDocument()).FirstOrDefault();
+            //Find all Movies
             var filter = Builders<Movies>.Filter.Empty;
             var MoviesFound = mongoCollection.Find(filter).ToList();
 
             InitializeComponent();
 
-            label1.Enabled = false;
-            label1.Visible = false;
+            DebugLabel.Enabled = false;
+            DebugLabel.Visible = false;
 
             dataGridView1.ReadOnly = true;
 
+            //Show Data
             dataGridView1.DataSource = MoviesFound;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
 
             //label1.Text = document.Aktorzy[0] + ", " + document.Aktorzy[1] + ", " + document.Aktorzy[2];
 
@@ -52,8 +60,50 @@ namespace MNDBProject
 
         private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
         {
-            label1.Visible = true;
-            label1.Text = dataGridView1.CurrentCell.RowIndex.ToString();
+            //DebugLabel.Visible = true;
+            DebugLabel.Text = dataGridView1.CurrentCell.RowIndex.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Clear the GridView
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+
+            //Like in setup
+            var mongoDatabase = dbClient.GetDatabase("Wypozyczalnia");
+            var mongoCollection = mongoDatabase.GetCollection<Clients>(Clients.ClientsDBName);
+            var filter = Builders<Clients>.Filter.Empty;
+            var ClientsFound = mongoCollection.Find(filter).ToList();
+
+            //Show Data
+            dataGridView1.DataSource = ClientsFound;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+
+            ShowClients.Enabled = false;
+            ShowMovies.Enabled = true;
+        }
+
+        private void ShowMovies_Click(object sender, EventArgs e)
+        {
+            //Clear the GridView
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+
+            //Like in setup
+            var mongoDatabase = dbClient.GetDatabase("Wypozyczalnia");
+            var mongoCollection = mongoDatabase.GetCollection<Movies>(Movies.MoviesDBName);
+            var filter = Builders<Movies>.Filter.Empty;
+            var MoviesFound = mongoCollection.Find(filter).ToList();
+
+            //Show Data
+            dataGridView1.DataSource = MoviesFound;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+
+            ShowClients.Enabled = true;
+            ShowMovies.Enabled = false;
         }
     }
 
@@ -62,21 +112,50 @@ namespace MNDBProject
         public static readonly string MoviesDBName = "Filmy";
 
         public int Id { get; set; }
+
         [BsonElement("tytul")]
         public string Tytul { get; set; }
+
         [BsonElement("gatunek")]
         public string[] Gatunek { get; set; }
+
         [BsonElement("rezyser")]
         public string Rezyser { get; set; }
+
         [BsonElement("czasTrwania")]
         public int CzasTrwania { get; set; }
+
         [BsonElement("ocena")]
         public double Ocena { get; set; }
+
         [BsonElement("opis")]
         public string Opis { get; set; }
+
         [BsonElement("aktorzy")]
         public string[] Aktorzy { get; set; }
+
         [BsonElement("dataDodania")]
         public DateTime DataDodania { get; set; }
+    }
+
+    public class Clients
+    {
+        public static readonly string ClientsDBName = "Klienci";
+        public ObjectId Id { get; set; }
+
+        [BsonElement("imie")]
+        public string Imie { get; set; }
+
+        [BsonElement("nazwisko")]
+        public string Nazwisko { get; set; }
+
+        [BsonElement("adres")]
+        public string Adres { get; set; }
+
+        [BsonElement("telefon")]
+        public string Telefon { get; set; }
+
+        [BsonElement("dataRejestracji")]
+        public DateTime DataRejestracji { get; set; }
     }
 }
