3486f8e80d0841d0699ce0b148e0cd6436a21ac7
GrimmyReaps <tomek1545@interia.pl>
2023-12-16 11:53:56 +0100

    Connection Estabilished
    The App can connect to MongoDB Database

--- MNDBProject/Form1.cs
+++ MNDBProject/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MongoDB.Driver;
 
 namespace MNDBProject
 {
@@ -15,11 +16,22 @@ namespace MNDBProject
         public Form1()
         {
             InitializeComponent();
+            MongoClient dbClient = new MongoClient("mongodb://localhost:27017");
+
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases on this server is: ");
+            foreach (var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             Close();
         }
+
+
     }
 }
