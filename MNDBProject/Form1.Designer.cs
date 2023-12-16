c98bf50717ddae3d607ec8d5f624f3f8c7019f60
GrimmyReaps <tomek1545@interia.pl>
2023-12-16 14:49:21 +0100

    The Data is displayed on the GridView

--- MNDBProject/Form1.Designer.cs
+++ MNDBProject/Form1.Designer.cs
@@ -29,12 +29,14 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(746, 451);
+            this.button1.Location = new System.Drawing.Point(1105, 451);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(91, 34);
             this.button1.TabIndex = 0;
@@ -42,32 +44,47 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // listBox1
+            // label1
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(3, 3);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(435, 484);
-            this.listBox1.TabIndex = 1;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1079, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Empty Sadge T_T";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 9);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1015, 476);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(849, 497);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(1208, 497);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- MNDBProject/Form1.cs
+++ MNDBProject/Form1.cs
@@ -7,24 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 
 namespace MNDBProject
 {
     public partial class Form1 : Form
     {
+        MongoClient dbClient = null;
         public Form1()
         {
+            dbClient = new MongoClient("mongodb://localhost:27017");
+            var mongoDatabase = dbClient.GetDatabase("Wypozyczalnia");
+            var mongoCollection = mongoDatabase.GetCollection<Movies>(Movies.MoviesDBName);
+            //var document = mongoCollection.Find(new BsonDocument()).FirstOrDefault();
+            var filter = Builders<Movies>.Filter.Empty;
+            var MoviesFound = mongoCollection.Find(filter).ToList();
+
             InitializeComponent();
-            MongoClient dbClient = new MongoClient("mongodb://localhost:27017");
 
-            var dbList = dbClient.ListDatabases().ToList();
+            label1.Enabled = false;
+            label1.Visible = false;
 
-            Console.WriteLine("The list of databases on this server is: ");
-            foreach (var db in dbList)
-            {
-                Console.WriteLine(db);
-            }
+            dataGridView1.ReadOnly = true;
+
+            dataGridView1.DataSource = MoviesFound;
+
+            //label1.Text = document.Aktorzy[0] + ", " + document.Aktorzy[1] + ", " + document.Aktorzy[2];
+
+            //var dbList = dbClient.ListDatabases().ToList();
+
+            //Console.WriteLine("The list of databases on this server is: ");
+            //foreach (var db in dbList)
+            //{
+            //    Console.WriteLine(db);
+            //}
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -32,6 +50,33 @@ namespace MNDBProject
             Close();
         }
 
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            label1.Visible = true;
+            label1.Text = dataGridView1.CurrentCell.RowIndex.ToString();
+        }
+    }
 
+    public class Movies
+    {
+        public static readonly string MoviesDBName = "Filmy";
+
+        public int Id { get; set; }
+        [BsonElement("tytul")]
+        public string Tytul { get; set; }
+        [BsonElement("gatunek")]
+        public string[] Gatunek { get; set; }
+        [BsonElement("rezyser")]
+        public string Rezyser { get; set; }
+        [BsonElement("czasTrwania")]
+        public int CzasTrwania { get; set; }
+        [BsonElement("ocena")]
+        public double Ocena { get; set; }
+        [BsonElement("opis")]
+        public string Opis { get; set; }
+        [BsonElement("aktorzy")]
+        public string[] Aktorzy { get; set; }
+        [BsonElement("dataDodania")]
+        public DateTime DataDodania { get; set; }
     }
 }
